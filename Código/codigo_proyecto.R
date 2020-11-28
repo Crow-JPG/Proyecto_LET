47dcac900040be822577efd8d85cb85fa1e7c177
Maximiliano Norbu <37166583+Crow-JPG@users.noreply.github.com>
2020-11-28 00:10:32 -0300

    Update codigo_proyecto.R

--- Código/codigo_proyecto.R
+++ Código/codigo_proyecto.R
@@ -1,6 +1,8 @@
-Datos = read_csv("C:/Users/PC/Desktop/u/LET/proyecto final/Base_de_datos_sin_procesar_excell.csv")
+Datos = read_csv("C:/Users/PC/Desktop/u/LET/proyecto_final/Base_de_datos_sin_procesar_excell.csv")
 Datos
 #plot(Datos$length,Datos$Birthweight, xlab = "Altura del bebé", ylab = "Peso del bebé", main = "Relación peso-altura", col = "red")
+
+
 
 Datos_A = Datos[,-1] #Datos_A serán mis datos modificados y aquí saco la variable ID pues no me importa identificar a los bebés en mi estudio, y puede dificultar hacer mis regresiones
 Datos_A = Datos_A[,-14]
@@ -11,6 +13,9 @@ for (i in 1:length(Datos_A$LowBirthWeight)){ #Aquí transformo la variable LowBi
 Datos_A2 = Datos_A[,-1]
 Datos_A2 = Datos_A2[,-1]
 Datos_A2 = Datos_A2[,-1]
+
+write.csv(Datos_A, file = "Datos_Actualizados1.csv")
+write.csv(Datos_A2, file = "Datos_Actualizados2.csv")
 
 #Datos_A2 es una versión con menos variables pues creo que no son rasgos externos que afecten al bebé
 
