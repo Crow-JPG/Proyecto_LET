1b916207d5e74545036dd97f109f5254075aa1ca
Maximiliano Norbu <37166583+Crow-JPG@users.noreply.github.com>
2020-12-14 20:44:15 -0300

    Update codigo_proyecto.R

--- Código/codigo_proyecto.R
+++ Código/codigo_proyecto.R
@@ -1,32 +1,32 @@
+library(readr)
 Datos = read_csv("C:/Users/PC/Desktop/u/LET/proyecto_final/Base_de_datos_sin_procesar_excell.csv")
-Datos
-#plot(Datos$length,Datos$Birthweight, xlab = "Altura del bebé", ylab = "Peso del bebé", main = "Relación peso-altura", col = "red")
-
-
-
-Datos_A = Datos[,-1] #Datos_A serán mis datos modificados y aquí saco la variable ID pues no me importa identificar a los bebés en mi estudio, y puede dificultar hacer mis regresiones
-Datos_A = Datos_A[,-14]
-for (i in 1:length(Datos_A$LowBirthWeight)){ #Aquí transformo la variable LowBirthWeight a números
-  if (Datos_A$LowBirthWeight[i]== "Low"){Datos_A$LowBirthWeight[i] = 0}
-  if (Datos_A$LowBirthWeight[i]== "Normal"){Datos_A$LowBirthWeight[i] = 1}
+Datos_A = Datos[,-17]
+Datos_A = Datos_A[,-1]
+Datos_A = Datos_A[,-15]
+nombres = c("Circunferencia_Cabeza", "Largo", "Peso", "Gestación", "Madre_Fumadora", "Edad_Madre", "Cigarrillos_Madre", "Altura_Madre", "Peso_Madre", "Edad_Padre", "Años_Educación_Padre", "Cigarrillos_Padre", "Altura_Padre", "Peso_Bajo_Bebé")
+names(Datos_A) = nombres
+Datos_A2 = cbind(Datos_A[,1:14])
+Datos_A3 = Datos_A2[,-1]
+Datos_A3 = Datos_A3[,-1]
+Datos_A3 = Datos_A3[,-1]
+Padre_Fumador = c()
+for (i in 1:length(Datos_A$Madre_Fumadora)){
+  if (Datos_A$Cigarrillos_Padre[i]== 0){Padre_Fumador[i] = 0}
+  if (Datos_A$Cigarrillos_Padre[i] > 0){Padre_Fumador[i] = 1}
 }
-Datos_A2 = Datos_A[,-1]
-Datos_A2 = Datos_A2[,-1]
-Datos_A2 = Datos_A2[,-1]
-
+nombre = "Padre_Fumador"
+names(Padre_Fumador) = nombre
+Datos_A3 = cbind(Datos_A3,Padre_Fumador)
+Max_Cigarrillos_Madre = max(Datos_A3$Cigarrillos_Madre)
+Datos_A3$Cigarrillos_Madre = Datos_A3$Cigarrillos_Madre/Max_Cigarrillos_Madre
+Max_Cigarrillos_Padre = max(Datos_A3$Cigarrillos_Padre)
+Datos_A3$Cigarrillos_Padre = Datos_A3$Cigarrillos_Padre/Max_Cigarrillos_Padre
+padres_fumadores = 0
+madres_fumadoras = 0
+for (i in 1:length(Datos_A$Madre_Fumadora)){
+  if (Datos_A3$Padre_Fumador[i]== 1){padres_fumadores = padres_fumadores + 1}
+  if (Datos_A3$Madre_Fumadora[i]== 1){madres_fumadoras = madres_fumadoras + 1}
+}
 write.csv(Datos_A, file = "Datos_Actualizados1.csv")
 write.csv(Datos_A2, file = "Datos_Actualizados2.csv")
-
-#Datos_A2 es una versión con menos variables pues creo que no son rasgos externos que afecten al bebé
-
-regresion_fumador = lm(LowBirthWeight ~ smoker, data = Datos_A)
-regresion_cigarros_madre = lm(LowBirthWeight ~ mnocig, data = Datos_A)
-regresion_cigarros_padre = lm(LowBirthWeight ~ fnocig, data = Datos_A)
-regresion_todo = lm(LowBirthWeight ~ ., data = Datos_A)
-regresion_util = lm(LowBirthWeight ~ ., data = Datos_A2)
-
-plot(regresion_fumador)
-plot(regresion_cigarros_madre)
-plot(regresion_cigarros_padre)
-plot(regresion_todo)
-plot(regresion_util)
+write.csv(Datos_A3, file = "Datos_Actualizados3.csv")
