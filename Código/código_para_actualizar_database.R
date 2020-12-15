be42cee8e91d4e433a3c7541f71cf8547c22ff68
Maximiliano Norbu <37166583+Crow-JPG@users.noreply.github.com>
2020-12-14 21:07:49 -0300

    Update código_para_actualizar_database.R

--- Código/código_para_actualizar_database.R
+++ Código/código_para_actualizar_database.R
@@ -1,9 +1,10 @@
 library(readr)
+
 Datos = read_csv("C:/Users/PC/Desktop/u/LET/proyecto_final/Base_de_datos_sin_procesar_excell.csv")
-Datos_A = Datos[,-17]
+Datos_A = Datos[,-17] 
 Datos_A = Datos_A[,-1]
 Datos_A = Datos_A[,-15]
-nombres = c("Circunferencia_Cabeza", "Largo", "Peso", "Gestación", "Madre_Fumadora", "Edad_Madre", "Cigarrillos_Madre", "Altura_Madre", "Peso_Madre", "Edad_Padre", "Años_Educación_Padre", "Cigarrillos_Padre", "Altura_Padre", "Peso_Bajo_Bebé")
+nombres = c("Circunferencia_Cabeza", "Largo", "Peso", "Gestacion", "Madre_Fumadora", "Edad_Madre", "Cigarrillos_Madre", "Altura_Madre", "Peso_Madre", "Edad_Padre", "Anios_Educación_Padre", "Cigarrillos_Padre", "Altura_Padre", "Peso_Bajo_Bebe")
 names(Datos_A) = nombres
 Datos_A2 = cbind(Datos_A[,1:14])
 Datos_A3 = Datos_A2[,-1]
@@ -18,15 +19,9 @@ nombre = "Padre_Fumador"
 names(Padre_Fumador) = nombre
 Datos_A3 = cbind(Datos_A3,Padre_Fumador)
 Max_Cigarrillos_Madre = max(Datos_A3$Cigarrillos_Madre)
-Datos_A3$Cigarrillos_Madre = Datos_A3$Cigarrillos_Madre/Max_Cigarrillos_Madre
 Max_Cigarrillos_Padre = max(Datos_A3$Cigarrillos_Padre)
+Datos_A3$Cigarrillos_Madre = Datos_A3$Cigarrillos_Madre/Max_Cigarrillos_Madre
 Datos_A3$Cigarrillos_Padre = Datos_A3$Cigarrillos_Padre/Max_Cigarrillos_Padre
-padres_fumadores = 0
-madres_fumadoras = 0
-for (i in 1:length(Datos_A$Madre_Fumadora)){
-  if (Datos_A3$Padre_Fumador[i]== 1){padres_fumadores = padres_fumadores + 1}
-  if (Datos_A3$Madre_Fumadora[i]== 1){madres_fumadoras = madres_fumadoras + 1}
-}
 write.csv(Datos_A, file = "Datos_Actualizados1.csv")
 write.csv(Datos_A2, file = "Datos_Actualizados2.csv")
 write.csv(Datos_A3, file = "Datos_Actualizados3.csv")
